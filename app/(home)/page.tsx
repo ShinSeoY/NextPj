abe8b0de1e65ac36f70e7c015b207ed494b53cf4
ShinSeoY <tjdudtls312@gmail.com>
2024-04-12 18:12:30 +0900

    - data fecth (csr, ssr)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,47 @@
-'use client';
-import { useEffect, useState } from 'react';
+import { Metadata } from 'next';
 
-export default function Home() {
-    const [isLoding, setIsLoading] = useState(true);
-    const [movies, setMovies] = useState();
-    const getMovies = async () => {
-        const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
-        const json = await response.json();
-        setMovies(json);
-        setIsLoading(false);
-        return json;
-    };
-    useEffect(() => {
-        getMovies();
-    }, []);
+export const metadata: Metadata = {
+    title: 'HOME',
+};
+
+const url = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+}
+export default async function HomePage() {
+    const movies = await getMovies();
+
     return (
         <div>
             <h1>HOME</h1>
-            {isLoding ? 'Loding..' : JSON.stringify(movies)}
+            {JSON.stringify(movies)}
         </div>
     );
 }
+
+// 'use client';
+// import { useEffect, useState } from 'react';
+
+// export default function Home() {
+//     const [isLoding, setIsLoading] = useState(true);
+//     const [movies, setMovies] = useState();
+//     const getMovies = async () => {
+//         const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
+//         const json = await response.json();
+//         setMovies(json);
+//         setIsLoading(false);
+//         return json;
+//     };
+//     useEffect(() => {
+//         getMovies();
+//     }, []);
+//     return (
+//         <div>
+//             <h1>HOME</h1>
+//             {isLoding ? 'Loding..' : JSON.stringify(movies)}
+//         </div>
+//     );
+// }
