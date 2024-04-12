f3223cf9fb0a23588b8986a196e8d83ce9794884
ShinSeoY <tjdudtls312@gmail.com>
2024-04-12 17:02:13 +0900

    - data fecth

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,23 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-    title: 'Home',
-};
+'use client';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+    const [isLoding, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState();
+    const getMovies = async () => {
+        const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
+        const json = await response.json();
+        setMovies(json);
+        setIsLoading(false);
+        return json;
+    };
+    useEffect(() => {
+        getMovies();
+    }, []);
     return (
         <div>
             <h1>HOME</h1>
+            {isLoding ? 'Loding..' : JSON.stringify(movies)}
         </div>
     );
 }
