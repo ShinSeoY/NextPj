c2e581c948714c3ed206bbbb67e8f84d030337cc
ShinSeoY <tjdudtls312@gmail.com>
2024-04-13 09:49:13 +0900

    - 병렬적 data fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,15 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
     title: 'HOME',
 };
 
-const url = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
-    const response = await fetch(url);
+    // await new Promise((r) => setTimeout(r, 1000));
+    const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
@@ -17,7 +19,11 @@ export default async function HomePage() {
     return (
         <div>
             <h1>HOME</h1>
-            {JSON.stringify(movies)}
+            {movies.map((movie: any) => (
+                <li key={movie.id}>
+                    <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
+            ))}
         </div>
     );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -3,7 +3,39 @@
 //     return <h1>MOVIE DETAIL</h1>;
 // }
 
-export default function MovieDetail({ params: { id } }: { params: { id: string } }) {
-    console.log(id);
-    return <h1>MOVIE DETAIL {id}</h1>;
+import { API_URL } from '@/app/(home)/page';
+import MovieInfo from '@/components/movie-info';
+import MovieVideos from '@/components/movie-videos';
+import { Suspense } from 'react';
+
+// async function getMovie(id: string) {
+//     await new Promise((r) => setTimeout(r, 5000));
+//     const response = await fetch(`${API_URL}/${id}`);
+//     return response.json();
+// }
+
+// async function getVideos(id: string) {
+//     await new Promise((r) => setTimeout(r, 3000));
+//     const response = await fetch(`${API_URL}/${id}/videos`);
+//     return response.json();
+// }
+
+export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+    // 방법1. promis all을 활용한 병렬적 data fetch
+    // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]); // 병렬적으로 함수를 실행해줌
+    // return <h1>{movie.title} </h1>;
+    //
+    // 방법2. 컴포넌트로 분리하여 Suspense를 활용한 병렬적 data fetch - 컴포넌트별로 로딩됨
+    return (
+        <div>
+            <Suspense fallback={<h1>Loding movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>Loding movie video</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
+        </div>
+    );
 }
+
+// #3.6 recap 하면 됨
