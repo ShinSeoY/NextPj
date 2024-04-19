b8c09de5343e7228d3c22eca2febd1d588255ebd
ShinSeoY <tjdudtls312@gmail.com>
2024-04-19 19:42:18 +0900

    - iframe css

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from '@/app/(home)/page';
+import potato from '../style/movie-info.module.css';
 
 async function getMovie(id: string) {
     await new Promise((r) => setTimeout(r, 5000));
@@ -8,6 +9,18 @@ async function getMovie(id: string) {
 export default async function MovieInfo({ id }: { id: string }) {
     // await new Promise((r) => setTimeout(r, 3000));
     // throw new Error('...new error');
-    const videos = await getMovie(id);
-    return <h6>{JSON.stringify(videos)}</h6>;
+    const movie = await getMovie(id);
+    return (
+        <div className={potato.container}>
+            <img className={potato.poster} src={movie.poster_path} />
+            <div className={potato.info}>
+                <h1 className={potato.title}>{movie.title}</h1>
+                <h3> * {movie.vote_average.toFixed(1)}</h3>
+                <p>{movie.overview}</p>
+                <a href={movie.homepage} target={'_black'}>
+                    Homepage &rarr;
+                </a>
+            </div>
+        </div>
+    );
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from '@/app/(home)/page';
+import styles from '../style/movie-videos.module.css';
 
 async function getVideos(id: string) {
     await new Promise((r) => setTimeout(r, 3000));
@@ -8,5 +9,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
     const videos = await getVideos(id);
-    return <h6>{JSON.stringify(videos)}</h6>;
+    return (
+        <div className={styles.container}>
+            {videos.map((video: any) => (
+                <iframe
+                    key={video.id}
+                    src={`https://youtube.com/embed/${video.key}`}
+                    title={video.name}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                />
+            ))}
+        </div>
+    );
 }
